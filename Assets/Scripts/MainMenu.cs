900174095059cb91b7cbd51fb7d9b72a25bfa8d9
Electrife <125890275+Electrife@users.noreply.github.com>
2025-12-17 21:48:17 +0100

    Main Menu start
    Functionality to load scenes with button clicks.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void PlayGame2()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void PlayGame3()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+}
