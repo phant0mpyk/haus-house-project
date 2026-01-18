ccdaa6880e6dc521fdc7a58159dfd90de842d2c5
smiling897 <lillyburkhardt@gmx.de>
2026-01-18 13:47:50 +0100

    added background music for the tutorial

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -8,9 +8,9 @@ public class TutorialController : MonoBehaviour
     public TextMeshProUGUI tutorialText;
 
     [TextArea(3, 5)]
-    public string[] tutorialLines;
+    public string[] tutorialLines; // public, so I can add the text in unity, those are the tutorial lines
 
-    public string SceneName;
+    public string SceneName; 
 
     private int currentLineIndex = 0;
 
@@ -30,7 +30,7 @@ public class TutorialController : MonoBehaviour
         }
         else
         {
-            //loading the game scene after text is done
+            //loading the game scene after text is done, this is good so I can add as much text as I want and the game recorgnizes once all the text is over it can now switch to the game, like this its easier to add lines
             SceneManager.LoadScene("CatNap");
         }
     }
